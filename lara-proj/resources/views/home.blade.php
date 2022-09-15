2332fc91bd2e7dbf50667c62fdb135d52a894026
Aleksandr Vasiljev <zaswet@gmail.com>
2022-09-15 11:57:47 +0300

    profilescontroller  and username input(db) added

--- lara-proj/resources/views/home.blade.php
+++ lara-proj/resources/views/home.blade.php
@@ -2,21 +2,39 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img class="p-5 rounded-circle" src="/svg/2.jpg" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Lara-Proj.KeK</h1>
             </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>123</strong>Posts</div>
+                <div class="pe-5"><strong>23k</strong>Followers</div>
+                <div class="pe-5"><strong>212</strong>Following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                Lara-Proj.KeK
+            </div>
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe veniam velit eos aliquid ea adipisci delectus? At molestias et quas rem pariatur tempore, dolorem consequuntur!
+            </div>
+            <div>
+                <a href="#">www.lara-proj.kek</a>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100 h-50" src="/img/1s.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100 h-50" src="/img/2s.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100 h-50" src="/img/3s.jpg" alt="">
         </div>
     </div>
 </div>
--- lara-proj/resources/views/layouts/app.blade.php
+++ lara-proj/resources/views/layouts/app.blade.php
@@ -20,8 +20,13 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div>
+                        <img  src="/svg/1.svg" style="height: 20px; padding-right: 20px; border-right: 1.2px solid" alt="logo">
+                    </div>
+                    <div style="padding-left: 20px">Lara-Proj.KeK</div>
+
+
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,35 +42,34 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->username }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
